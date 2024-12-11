00aa8f941b3064dcf1c5ec469667020b3d436ced
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-11 15:10:47 +0100

    Added Landing page information

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,17 +1,35 @@
 import styled from "styled-components";
-
-const StyledBtn = styled.button({
-  fontSize: "1.5rem",
-  background: "red",
-  color: "white",
-});
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-      <StyledBtn>Styled btn</StyledBtn>
-    </div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab at
+            delectus eaque a, quod ex itaque. Illum dicta harum alias et dolores
+            quas, unde quasi? Mollitia numquam deleniti earum cumque.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 export default Landing;
