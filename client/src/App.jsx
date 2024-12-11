87dd060a7362ed8eb9692ad7724ea0dfd500fd73
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-11 13:49:31 +0100

    Added all pages that will be needed (hopefully)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeLayout from "./pages/HomeLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+]);
+
 const App = () => {
-  return <h1>Jobify App</h1>;
+  return <RouterProvider router={router} />;
 };
 export default App;
