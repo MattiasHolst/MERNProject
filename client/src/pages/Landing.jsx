50a19c7bca91c0fe2c13ce12b0895866132f8d05
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-11 14:46:03 +0100

    Added styled component package and tried to make a styled button for now

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,17 @@
+import styled from "styled-components";
+
+const StyledBtn = styled.button({
+  fontSize: "1.5rem",
+  background: "red",
+  color: "white",
+});
+
 const Landing = () => {
   return (
-    <h1>Landing</h1>
-  )
-}
-export default Landing
+    <div>
+      <h1>Landing</h1>
+      <StyledBtn>Styled btn</StyledBtn>
+    </div>
+  );
+};
+export default Landing;
