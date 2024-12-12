2e87c1f37eceff2c1e360416496eb39c72333207
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-12 12:50:11 +0100

    Fixed minor eslint config and added structure to big sidebar

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import links from "../utils/links";
 import { useDashboardContext } from "../pages/DashboardLayout";
 
-const NavLinks = () => {
+const NavLinks = ({ isBigSidebar }) => {
   const { toggleSidebar, user } = useDashboardContext();
   return (
     <div className="nav-links">
@@ -13,7 +13,7 @@ const NavLinks = () => {
             to={path}
             key={text}
             className="nav-link"
-            onClick={toggleSidebar}
+            onClick={isBigSidebar ? null : toggleSidebar}
             end
           >
             <span className="icon">{icon}</span>
