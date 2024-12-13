207e5216c14ade3b606cc555b09e70469fc57628
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-13 15:36:18 +0100

    All job endpoints using the DB now

--- jobify/controllers/jobController.js
+++ jobify/controllers/jobController.js
@@ -1,10 +1,5 @@
 import Job from "../models/JobModel.js";
-import { nanoid } from "nanoid";
 
-let jobs = [
-  { id: nanoid(), company: "apple", position: "front-end" },
-  { id: nanoid(), company: "google", position: "back-end" },
-];
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
@@ -30,29 +25,24 @@ export const createJob = async (req, res) => {
 
 // UPDATE JOB
 export const updateJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res
-      .status(400)
-      .json({ message: "Please provide company and position" });
-  }
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!updatedJob) {
     return res.status(404).json({ message: `No job with id ${id}` });
   }
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ message: "Job updated", job });
+
+  res.status(200).json({ message: "Job updated", job: updatedJob });
 };
 
 // DELETE JOB
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+  const removedJob = await Job.findByIdAndDelete(id);
+  if (!removedJob) {
     return res.status(404).json({ message: `No job with id ${id}` });
   }
-  jobs = jobs.filter((job) => job.id !== id);
-  res.status(200).json({ message: "Job deleted" });
+
+  res.status(200).json({ message: "Job deleted", job: removedJob });
 };
