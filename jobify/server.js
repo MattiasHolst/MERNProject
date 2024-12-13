e0921310ba23000803f7a3ed8e31cf63e6da5967
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-13 11:01:05 +0100

    Updated packages and added the first real api route, with hardcoded data though

--- jobify/server.js
+++ jobify/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
+import { nanoid } from "nanoid";
 
 dotenv.config();
 
 const app = express();
+
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -13,6 +19,10 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({jobs});
 });
 
 app.post("/", (req, res) => {
