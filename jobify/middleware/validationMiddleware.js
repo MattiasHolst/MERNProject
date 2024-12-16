180e80b40e1f30fcd9e44918904c708d65398ba3
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-16 13:31:43 +0100

    Added check in validateIdParams to see wether or not the user is admin and the user is the same as the one who created the object before doing anything

--- jobify/middleware/validationMiddleware.js
+++ jobify/middleware/validationMiddleware.js
@@ -1,5 +1,9 @@
 import { body, param, validationResult } from "express-validator";
-import { BadRequestError, NotFoundError } from "../errors/customErrors.js";
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+} from "../errors/customErrors.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import mongoose from "mongoose";
 import Job from "../models/JobModel.js";
@@ -14,6 +18,9 @@ const withValidationErrors = (validateValues) => {
         const errorMessages = errors.array().map((error) => error.msg);
         if (errorMessages[0].startsWith("No job")) {
           throw new NotFoundError(errorMessages);
+        }
+        if (errorMessages[0].startsWith("not authroized")) {
+          throw new UnauthorizedError("not authroized to access this route");
         }
         throw new BadRequestError(errorMessages);
       }
@@ -41,13 +48,17 @@ export const validateJobInput = withValidationErrors([
 ]);
 
 export const validateIdParams = withValidationErrors([
-  param("id").custom(async (value) => {
+  param("id").custom(async (value, { req }) => {
     const isValidId = mongoose.Types.ObjectId.isValid(value);
     if (!isValidId) throw new Error("Invalid MongoDB id");
     const job = await Job.findById(value);
     if (!job) throw new Error(`No job with id ${value}`);
+    const { userId, role } = req.user;
+    const isAdmin = role === "admin";
+    const isOwner = userId === job.createdBy.toString();
+    if (!isAdmin && !isOwner)
+      throw new UnauthorizedError("not authroized to access this route");
   }),
-  //.isMongoId()
 ]);
 
 /*
