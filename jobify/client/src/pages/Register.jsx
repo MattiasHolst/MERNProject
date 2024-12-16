7f01836c4e37181ceb594756b310dce4fc8737a5
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-16 15:26:42 +0100

    Added a test action

--- jobify/client/src/pages/Register.jsx
+++ jobify/client/src/pages/Register.jsx
@@ -1,14 +1,26 @@
-import { Link } from "react-router-dom";
+import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
+
+export const action = async (data) => {
+  console.log(data);
+
+  return null;
+};
+
 const Register = () => {
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
         <FormRow type="text" name="name" defaultValue="John" />
-        <FormRow type="text" name="lastName" labelText="last name" defaultValue="Doe" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="Doe"
+        />
         <FormRow type="text" name="location" defaultValue="Earth" />
         <FormRow type="email" name="email" defaultValue="John@doe.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
@@ -21,7 +33,7 @@ const Register = () => {
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
