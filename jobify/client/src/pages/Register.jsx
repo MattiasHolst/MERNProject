2d9e0b428e9d3d10530ed4e7283acb5593b67419
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-17 09:18:10 +0100

    Added useNavigation to register to keep track on the state

--- jobify/client/src/pages/Register.jsx
+++ jobify/client/src/pages/Register.jsx
@@ -17,6 +17,10 @@ export const action = async ({ request }) => {
 };
 
 const Register = () => {
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -32,8 +36,8 @@ const Register = () => {
         <FormRow type="text" name="location" defaultValue="Earth" />
         <FormRow type="email" name="email" defaultValue="John@doe.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
-        <button type="submit" className="btn btn-block">
-          Submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "Submit"}
         </button>
         <p>
           Already a member?
