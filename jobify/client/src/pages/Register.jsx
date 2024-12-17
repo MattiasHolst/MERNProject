9702e570036f76207a1edbbea2ff37577c5abf73
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-17 09:25:16 +0100

    Added react-toastify to show toasts when registered is being fired

--- jobify/client/src/pages/Register.jsx
+++ jobify/client/src/pages/Register.jsx
@@ -2,16 +2,18 @@ import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from "../utils/customFetch.js";
+import { toast } from "react-toastify";
+
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
   try {
     await customFetch.post("/auth/register", data);
+    toast.success("Registration successful");
     return redirect("/login");
   } catch (error) {
-    console.log(error);
-
+    toast.error(error?.response?.data?.message);
     return error;
   }
 };
