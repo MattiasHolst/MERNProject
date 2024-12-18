562b08abfb5d342a6cd305b4d0203bae68a3d802
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-18 10:32:01 +0100

    Added delete job functionallity

--- jobify/client/src/pages/DeleteJob.jsx
+++ jobify/client/src/pages/DeleteJob.jsx
@@ -1,6 +1,19 @@
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch.js";
+import { redirect } from "react-router-dom";
+
+export const action = async ({ params }) => {
+  try {
+    const { id } = params;
+    await customFetch.delete(`/jobs/${id}`);
+    toast.success("Job deleted successfully");
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+  }
+  return redirect("../all-jobs");
+};
+
 const DeleteJob = () => {
-  return (
-    <h1>DeleteJob</h1>
-  )
-}
-export default DeleteJob
+  return <h1>DeleteJob</h1>;
+};
+export default DeleteJob;
