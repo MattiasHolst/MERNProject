5fed2a78c67b00ae4e13c89edc79d6ebb985673d
Mattias Holst <mattias.holst@nexergroup.com>
2025-01-07 12:37:52 +0100

    Added backend support to change the sorting with query params

--- jobify/controllers/jobController.js
+++ jobify/controllers/jobController.js
@@ -5,7 +5,7 @@ import day from "dayjs";
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
-  const { search, jobStatus, jobType } = req.query;
+  const { search, jobStatus, jobType, sort } = req.query;
 
   const queryObject = {
     createdBy: req.user.userId,
@@ -18,14 +18,23 @@ export const getAllJobs = async (req, res) => {
     ];
   }
 
-  if(jobStatus && jobStatus !== 'all'){
+  if (jobStatus && jobStatus !== "all") {
     queryObject.jobStatus = jobStatus;
   }
-  if(jobType && jobType !== 'all'){
+  if (jobType && jobType !== "all") {
     queryObject.jobType = jobType;
   }
 
-  const jobs = await Job.find(queryObject);
+  const sortOptions = {
+    newest: "-createdAt",
+    oldest: "createdAt",
+    "a-z": "position",
+    "z-a": "-position",
+  };
+
+  const sortKey = sortOptions[sort] || sortOptions.newest;
+
+  const jobs = await Job.find(queryObject).sort(sortKey);
   res.status(StatusCodes.OK).json({ jobs });
 };
 
