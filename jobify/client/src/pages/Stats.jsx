120f244b91f0b76121a4079ff54bc3e476cf3bd4
Mattias Holst <mattias.holst@nexergroup.com>
2025-01-09 08:48:33 +0100

    Added react query to stats page

--- jobify/client/src/pages/Stats.jsx
+++ jobify/client/src/pages/Stats.jsx
@@ -1,15 +1,25 @@
 import { useLoaderData } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import { ChartsContainer, StatsContainer } from "../components";
+import { useQuery } from "@tanstack/react-query";
 
-export const loader = async () => {
-  const response = await customFetch.get("/jobs/stats");
-  return response.data;
+const statsQuery = {
+  queryKey: ["stats"],
+  queryFn: async () => {
+    const response = await customFetch.get("/jobs/stats");
+    return response.data;
+  },
+};
+
+export const loader = (queryClient) => async () => {
+  const data = await queryClient.ensureQueryData(statsQuery);
+  return null;
 };
 
 const Stats = () => {
-  const { defaultStats, monthlyApplications } = useLoaderData();
+  const { data } = useQuery(statsQuery);
 
+  const { defaultStats, monthlyApplications } = data;
   return (
     <>
       <StatsContainer defaultStats={defaultStats} />
