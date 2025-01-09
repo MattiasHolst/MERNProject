8946e7039d1b22982d75c8d6c682c65be11c471a
Mattias Holst <mattias.holst@nexergroup.com>
2025-01-09 10:01:27 +0100

    Added react query to alljobs and single job and also added invalidateQueries when creating/editing/deleting jobs

--- jobify/client/src/pages/DeleteJob.jsx
+++ jobify/client/src/pages/DeleteJob.jsx
@@ -2,10 +2,11 @@ import { toast } from "react-toastify";
 import customFetch from "../utils/customFetch.js";
 import { redirect } from "react-router-dom";
 
-export const action = async ({ params }) => {
+export const action = (queryClient) => async ({ params }) => {
   try {
     const { id } = params;
     await customFetch.delete(`/jobs/${id}`);
+    queryClient.invalidateQueries(['jobs']);
     toast.success("Job deleted successfully");
   } catch (error) {
     toast.error(error?.response?.data?.message);
--- jobify/client/src/pages/EditJob.jsx
+++ jobify/client/src/pages/EditJob.jsx
@@ -1,39 +1,57 @@
 import { FormRow, FormRowSelect, SubmitBtn } from "../components";
 import Wrapper from "../assets/wrappers/DashboardFormPage";
-import { useLoaderData } from "react-router-dom";
 import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useLoaderData } from "react-router-dom";
 import { toast } from "react-toastify";
 import customFetch from "../utils/customFetch";
+import { useQuery } from "@tanstack/react-query";
 
-export const action = async ({ request, params }) => {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  const { id } = params;
-
-  try {
-    await customFetch.patch(`/jobs/${id}`, data);
-    toast.success("Updated job successfully");
-    return redirect("../all-jobs");
-  } catch (error) {
-    toast.error(error?.response?.data?.message);
-    return error;
-  }
+const jobQuery = (id) => {
+  return {
+    queryKey: ["job", id],
+    queryFn: async () => {
+      const { data } = await customFetch.get(`/jobs/${id}`);
+      return data;
+    },
+  };
 };
 
-export const loader = async ({ params }) => {
-  try {
+export const action =
+  (queryClient) =>
+  async ({ request, params }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
     const { id } = params;
-    const { data } = await customFetch.get(`/jobs/${id}`);
-    return data;
-  } catch (error) {
-    toast.error(error?.response?.data?.message);
-    return redirect("../all-jobs");
-  }
-};
+
+    try {
+      await customFetch.patch(`/jobs/${id}`, data);
+      queryClient.invalidateQueries(["jobs"]);
+      toast.success("Updated job successfully");
+      return redirect("../all-jobs");
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+      return error;
+    }
+  };
+
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    try {
+      const { id } = params;
+      const { data } = await queryClient.ensureQueryData(jobQuery(id));
+      return id;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+      return redirect("../all-jobs");
+    }
+  };
 
 const EditJob = () => {
-  const { job } = useLoaderData();
+  const id = useLoaderData();
+  const {
+    data: { job },
+  } = useQuery(jobQuery(id));
   return (
     <Wrapper>
       <Form method="post" className="form">
